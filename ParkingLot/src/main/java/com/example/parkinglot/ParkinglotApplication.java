98db173fe1f1e8d4e80ce8cf788b4cad61de9722
Sanjiv <sanjeev.oggy@gmail.com>
2022-04-09 15:31:40 +0530

    Edit SpringApp

--- ParkingLot/src/main/java/com/example/parkinglot/ParkinglotApplication.java
+++ ParkingLot/src/main/java/com/example/parkinglot/ParkinglotApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ParkinglotApplication {
+public class ParkingLotApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ParkinglotApplication.class, args);
+		SpringApplication.run(ParkingLotApplication.class, args);
 	}
 
 }
